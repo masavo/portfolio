d300da0000088b0eb2164d147984951a13205d1e
Masanori Hiroki <hiroki.masanori06@gmail.com>
2018-10-24 17:33:44 +0900

    add: ripple effectのjsとclassをwq追加

---
+++ app/assets/javascripts/view-motion.js
@@ -0,0 +1,42 @@
+const ready = () => {
+    // $('.fadein').fadeIn(800);
+    $('.fadein').on('inview', (event, isInView, visiblePartX, visiblePartY) => {
+        const $this = $(event.currentTarget);
+        // $this.fadeIn(800);
+        isInView ? $this.stop().addClass('mv02') : null;
+    });
+    var $clickable = $('.ripple');
+
+    /* mousedownだと直ぐに発動し、clickだとマウスボタンを離した時に発動する */
+    $clickable.on('mousedown', function (e) {
+        var _self = this;
+        var x = e.offsetX;
+        var y = e.offsetY;
+
+        var $effect = $(_self).find('.ripple-effect');
+        var w = $effect.width();
+        var h = $effect.height();
+
+        /* クリックした座標を中心とする */
+        $effect.css({
+            left: x - w / 2,
+            top: y - h / 2
+        });
+
+        /* jsではclassの付け替えをするだけ */
+        if (!$effect.hasClass('is-show')) {
+            $effect.addClass('is-show');
+
+            /*
+                * エフェクトアニメーションが終わったらclassを削除する
+                * ここでは、単純にcssで設定するdurationと時間を合わせているだけですが
+                * keyframes終了のイベント(AnimationEnd)が取れるかと思うので、それで対応した方が良いかも
+                */
+            setTimeout(function () {
+                $effect.removeClass('is-show');
+            }, 750);
+        }
+        return false;
+    });
+};
+$(document).on('turbolinks:load', ready);
