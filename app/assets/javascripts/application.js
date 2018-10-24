f1c9abfc0e2e1ef42ca75ef010f6dbc37288c93d
Masanori Hiroki <hiroki.masanori06@gmail.com>
2018-10-24 12:35:28 +0900

    add: fadeinを実現するためのjsライブラリを追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery-inview
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require view-motion
+//= require turbolinks
+//= require_tree .
