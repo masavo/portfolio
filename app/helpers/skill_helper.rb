e23998c97104881c12c612dbe078fbf4405f2bb6
Masanori Hiroki <hiroki.masanori06@gmail.com>
2018-10-24 17:29:54 +0900

    update: pngがなかったときにjpg探すように変更

--- app/helpers/skill_helper.rb
+++ app/helpers/skill_helper.rb
@@ -1,6 +1,12 @@
 module SkillHelper
     def to_image_file_path(str)
-        file_path = "skill/#{str.gsub(/ /,"_").downcase}.jpg"
-        if File.exist?("#{Rails.root}/app/assets/images/#{file_path}") then file_path else "default.jpg" end
+        file_path = "skill/#{str.gsub(/ /,"_").downcase}"
+        if File.exist?("#{Rails.root}/app/assets/images/#{file_path}.png")
+             file_path+".png"
+        elsif File.exist?("#{Rails.root}/app/assets/images/#{file_path}.jpg")
+             file_path+".jpg"
+        else
+            "default.jpg"
+        end
     end
 end
