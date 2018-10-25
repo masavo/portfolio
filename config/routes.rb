bb601c83674f17137184a2b4a5567a6328156da7
Masanori Hiroki <hiroki.masanori06@gmail.com>
2018-10-25 10:39:50 +0900

    update: careerのリンクを消した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/career', to: 'career#career'
+  # get '/career', to: 'career#career'
   get '/skill', to: 'skill#skill'
   get '/product', to: 'product#product'
   root "index#index"
