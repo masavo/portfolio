bdccee6899d02bcdb2840d6fcfac0cc3c10ee66e
hiroki06 <hiroki.masanori06@gmail.com>
2019-10-05 10:31:46 +0900

    Update skill factory

--- spec/factories/skills.rb
+++ spec/factories/skills.rb
@@ -11,9 +11,13 @@
 #  career_length :string(255)
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
-#
 
+require 'faker'
 FactoryBot.define do
   factory :skill do
+    title Faker::ProgrammingLanguage
+    experience 'experience'
+    career_length '1年'
+    category Category.new(name: 'category')
   end
 end
